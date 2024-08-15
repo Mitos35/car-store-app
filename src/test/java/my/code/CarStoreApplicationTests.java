15d8bc5038fe257068e97bdb660f1f2f94d117c2
Mitos <MiToSSH@gmail.com>
2024-08-15 23:24:39 +0300

    change to test

---
+++ src/test/java/my/code/CarStoreApplicationTests.java
@@ -0,0 +1,12 @@
+package my.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
